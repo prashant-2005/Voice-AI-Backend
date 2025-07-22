3f312e2752fcbf03524f27e60b865af2913544e4
Prashant Raj <prashantraj01122@gmail.com>
2025-07-22 14:25:37 +0530

    .

--- src/middlewares/isAuth.js
+++ src/middlewares/isAuth.js
@@ -6,7 +6,6 @@ dotenv.config()
 const isAuth = async (req,res,next)=>{
     try {
         const token = req.cookies.token 
-
         if(!token){
             return res.status(400).json({
                 message:"token not found"
